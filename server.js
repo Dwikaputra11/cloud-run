776e03d7f674a37931b5a57ff8b950c953cb2f14
Dwika Putra <72721412+Dwikaputra11@users.noreply.github.com>
2023-03-13 11:17:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Welcome Cloud Run!');
+	res.send('Welcome Home!');
 });
 
 const PORT = process.env.PORT || 8080;
